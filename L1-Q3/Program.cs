e181ae8c831aa6c39d453378dfe46c71ef6e312a
sagar26197@gmail.com <sagar26197@gmail.com>
2019-01-22 18:26:35 -0500

    V2

--- L1-Q3/Program.cs
+++ L1-Q3/Program.cs
@@ -6,7 +6,30 @@ namespace L1_Q3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter 3 Numbers: ");
+            int x, y, z;
+            Console.WriteLine("Input first number");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input second number");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input third number");
+            z = int.Parse(Console.ReadLine());
+
+            if ( x >= y && x >= z)
+            {
+                Console.WriteLine("The greatest number is = {0}", +x);
+                Console.ReadLine();
+            }
+            else if (y >= x && y >= z)
+            {
+                Console.WriteLine("The greatest number is = {0}", +y);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("The greatest number is = {0}", +z);
+                Console.ReadLine();
+            }
         }
     }
 }
